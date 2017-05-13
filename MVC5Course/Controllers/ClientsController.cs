5feb03794a5065680dd96fc3d21d6b441e46ccae
JJ <jerrychoubear@gmail.com>
2017-05-14 00:48:07 +0800

    練習5/13的BatchUpdate。

---
+++ MVC5Course/Controllers/ClientsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
+
+namespace MVC5Course.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        // GET: Clients
+        public ActionResult BatchUpdate()
+        {
+            GetClients();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BatchUpdate(ClientsBatchUpdateVM[] items)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var item in items)
+                {
+                    var client = db.Clients.Find(item.ClientId);
+                    client.FirstName = item.FirstName;
+                    client.MiddleName = item.MiddleName;
+                    client.LastName = item.LastName;
+                }
+                db.SaveChanges();
+
+                return RedirectToAction("BatchUpdate");
+            }
+
+            GetClients();
+
+            return View();
+        }
+
+        private void GetClients()
+        {
+            var clients = db.Clients.Include(c => c.Occupation).Take(10);
+            ViewData.Model = clients;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC5Course/Models/ViewModels/ClientsBatchUpdateVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.ViewModels
+{
+    public class ClientsBatchUpdateVM
+    {
+        [Required]
+        public int ClientId { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "欄位長度不得大於 40 個字元")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "欄位長度不得大於 40 個字元")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "欄位長度不得大於 40 個字元")]
+        public string LastName { get; set; }
+    }
+}
